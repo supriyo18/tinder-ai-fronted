e91a16be21afdf330b1d56e102c35325ed0e39c5
supriyo18 <mukherjee.supriyo18@gmail.com>
2024-08-09 04:03:55 +0530

    working in progress

--- tinder-ai-frontend/src/App.jsx
+++ tinder-ai-frontend/src/App.jsx
@@ -1,10 +1,35 @@
-function App() {
+import './App.css';
+import { User, MessageCircle } from 'lucide-react';
 
+const ProfileSelector = () => {
+  return (
+    <div className='rounded-lg overflow-hidden bg-white shadow-lg'>
+      <div className='relative p-4'>
+        <img
+          src="http://localhost:8080/images/0b1273d4-ab2f-4edd-858b-6f1ff1071fb9.jpg"
+          alt="Profile"  // Adding alt attribute
+          className="w-full h-auto"  // Ensure the image fits the container
+        />
+        <div className='absolute bottom-0 left-0 right-0 text-white bg-gradient-to-t from-gray-600 to-transparent'>
+          <h2 className='text-3xl font-semibold'>Shreyaa , 29</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function App() {
   return (
     <>
-      <h1>HI AI APP</h1>
+      <div className='max-w-md mx-auto pt-8'>
+        <nav className='flex justify-between space-x-4'>
+          <User />
+          <MessageCircle />
+        </nav>
+        <ProfileSelector />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tinder-ai-frontend/vite.config.js
+++ tinder-ai-frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()]
-    }
-  }
+  plugins: [react()]
 })
