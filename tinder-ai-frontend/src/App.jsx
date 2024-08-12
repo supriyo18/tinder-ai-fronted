5ffa9f718ac3660bc7081d1164ee957a79d79321
supriyo18 <mukherjee.supriyo18@gmail.com>
2024-08-12 18:27:29 +0530

    like dislike profile implementation

--- tinder-ai-frontend/src/App.jsx
+++ tinder-ai-frontend/src/App.jsx
@@ -1,18 +1,51 @@
 import './App.css';
 import { User, MessageCircle } from 'lucide-react';
 import ProfileSelector from './components/ProfileSelector';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MatchList from './components/MatchList';
 import ChatScreen from './components/ChatScreen';
 
+const fetchRandomProfile = async () => {
+  const response = await fetch('http://localhost:8080/profiles/random');
+  if (!response.ok) {
+    throw new Error('Failed to fetch profile');
+  }
+  return response.json();
+}
+
+
 function App() {
+
+  const loadRandomProfile = async () => {
+    try {
+      const profile = await fetchRandomProfile();
+      SetCurrentProfile(profile);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+
+  useEffect(() => {
+    loadRandomProfile();
+  }, [])
+
   const [currentScreen, SetCurrentScreen] = useState('profile')
 
+  const [currentProfile, SetCurrentProfile] = useState(null)
+
+  const onSwipe = (direction) => {
+    if (direction == 'right') {
+      console.log('Liked')
+    }
+    loadRandomProfile();
+  }
   const renderScreen = () => {
 
     switch (currentScreen) {
       case 'profile':
-        return <ProfileSelector />;
+        return <ProfileSelector profile={currentProfile} onSwipe={onSwipe} />;
       case 'matches':
         return <MatchList onSelectMatch={() => SetCurrentScreen('chat')} />;
       case 'chat':
